cd18a456c94ee6e25d8556e2272173e17b6aae8f
Nick Peters <petersnicholas2@gmail.com>
2018-07-29 15:31:58 -0700

    added beautifulsoup to parse top link from homepage

--- twython_tweet.py
+++ twython_tweet.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python
 import sys
 from twython import Twython
+from bs4 import BeautifulSoup
+import requests
 
-tweetStr = raw_input("Input Tweet: ")
+#web scraping
+page_link = 'https://thoughtcosmonaut.com/'
+page_response = requests.get(page_link, timeout=5)
+page_content = BeautifulSoup(page_response.content, "html.parser")
 
-# your twitter consumer and access information goes here
+new_article = page_content.findAll("a", {"class": "top-article-title"})
+
+#twitter posting
+tweetStr = new_article
+
+# twitter consumer and access information
 apiKey = 'HT1H1WkUhl27GQR73ZMGqTZZ6'
 apiSecret = 'lSKbBiDu9THEXIrZJvCpcUTIg3Am0TEZb3rPyWGitj4xI3hT29'
 accessToken = '1023648868276162560-7o2uHYhafWz4I6lYWqLmz5DeBPPllc'
