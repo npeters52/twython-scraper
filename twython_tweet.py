058487369f3105cc0ee4c9564efb28fedfcc3669
Nick Peters <petersnicholas2@gmail.com>
2018-07-29 15:56:25 -0700

    troubleshooting tweet with link to new article

--- twython_tweet.py
+++ twython_tweet.py
@@ -14,8 +14,9 @@ new_article = page_content.findAll("href", {"class": "blog-new-article-parse"})
 print(new_article)
 
 #twitter posting
-tweetStr = raw_input("Input Tweet: ")
-# your twitter consumer and access information goes here
+tweetStr = "Check out this new article!"
+
+# twitter consumer and access information
 apiKey = 'HT1H1WkUhl27GQR73ZMGqTZZ6'
 apiSecret = 'lSKbBiDu9THEXIrZJvCpcUTIg3Am0TEZb3rPyWGitj4xI3hT29'
 accessToken = '1023648868276162560-7o2uHYhafWz4I6lYWqLmz5DeBPPllc'
@@ -23,6 +24,8 @@ accessTokenSecret = '5TLFlMWTENccbxd2EgiV0qCODK4CmWNbeRRb7m2SuRXhD'
 
 api = Twython(apiKey,apiSecret,accessToken,accessTokenSecret)
 
-api.update_status(status=tweetStr)
+response = api.upload_media(media=new_article)
+
+api.update_status(status=tweetStr, media_ids=[response['media_id']])
 
 print("Tweeted: " + tweetStr)
